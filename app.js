1115b9733c3b25b4bab10dfa6566d3fa5723f9e8
marifra61 <107248064+marifra61@users.noreply.github.com>
2026-01-21 22:54:46 -0500

    fix: ensure global callback for Google Maps and fix module paths

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿import { initLogLegendsMap } from './map.js';
+﻿import { initLogLegendsMap } from 'https://marifra61.github.io/LogLegends/map.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("LogLegends App Started");
